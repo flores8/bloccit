fe8e5b8db43119a37b3663cb9d22ced15e3f84c0
flores8 <flores.lauralee@gmail.com>
2014-04-10 14:42:05 -0700

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+== Bloccit is a Redit clone that I'm using to more fully learn the fundamentals of web development and Rails 4.  
+
+I am making it with my mentor [Michael](http://michaelpell.com/) at [Bloc](http://bloc.io).
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Bloccit::Application.config.secret_key_base = 'e92566f04e0b08d131e3269e54383ddff52e468800c50cc960d13ad36923ef38adf25b2d0c79fc71f2a804fd61b988129cc4e64b1276f3fbdd5f44cd03cfeb0d'
