ffb02f9ee21d7a7e63de495c7fe2a44bf33676db
flores8 <flores.lauralee@gmail.com>
2014-04-14 12:35:45 -0700

    Added uploading images functionality but running into a nil error.

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,6 +4,7 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable, :confirmable
   has_many :posts
+  mount_uploader :avatar, AvatarUploader
 
   def role?(base_role)
   	role == base_role.to_s
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140412212122) do
+ActiveRecord::Schema.define(version: 20140414165123) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -64,6 +64,7 @@ ActiveRecord::Schema.define(version: 20140412212122) do
     t.datetime "created_at"
     t.datetime "updated_at"
     t.string   "role"
+    t.string   "avatar"
   end
 
   add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
