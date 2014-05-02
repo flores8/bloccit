9cb4a52552fe7e5efa483ba68205ab24019025e6
flores8 <flores.lauralee@gmail.com>
2014-05-01 22:48:45 -0700

    Added footer

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,22 @@
 class ApplicationController < ActionController::Base
 	include Pundit
-
+ 
 	rescue_from Pundit::NotAuthorizedError do |exception|
 		redirect_to root_url, alert: exception.message
 	end
+
+  def after_sign_in_path_for(resource)
+    topics_path
+  end
+
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
-  def after_sign_in_path_for(resource)
-  	topics_path
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << :name
   end
 end
